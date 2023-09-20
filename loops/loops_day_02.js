c2861ebd4def130ce74595d4c696d11bea9ce480
sonic11829 <sethneal15@ymail.com>
2023-09-19 20:43:55 -0400

    added comments

--- loops/loops_day_02.js
+++ loops/loops_day_02.js
@@ -2,6 +2,8 @@
 const dogs = ['Great Dane', 'Labrador', 'German Shepherd']
 const oddNumbers = [1, 3, 5, 7, 9]
 
+/* display1 is being used to show the position of each element in an array */
+
 display1 = function(arr){
   for(let i = 0; i < arr.length; i++){
     console.log(`The position of this element is ${i}`)
@@ -10,6 +12,8 @@ display1 = function(arr){
 
 display1(dogs)
 display1(oddNumbers)
+
+/* display2 is being used to display the element in an array along with the position that element is in. */
 
 display2 = function(arr){
   for(let i = 0; i < arr.length; i++){
