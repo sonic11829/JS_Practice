832d9f0b9fda55c12216b49a1ff1164b6096f448
sonic11829 <sethneal15@ymail.com>
2023-09-20 21:32:13 -0400

    created second function and tested, works as well

--- loops/loopsDay03.js
+++ loops/loopsDay03.js
@@ -10,3 +10,12 @@ display1 = function(arr){
 
 display1(trees)
 display1(evenNumbers)
+
+display2 = function(arr){
+  for(let i = 0; i < arr.length; i++){
+    console.log(`${arr[i]} is in position ${i} of this array`)
+  }
+}
+
+display2(trees)
+display2(evenNumbers)
