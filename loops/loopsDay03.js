7e31f73710a9e2d0399d9ea27e7de2ee66167767
sonic11829 <sethneal15@ymail.com>
2023-09-20 21:32:25 -0400

    added comments

--- loops/loopsDay03.js
+++ loops/loopsDay03.js
@@ -2,6 +2,8 @@
 const trees = ['oak', 'birch', 'maple', 'dog', 'pine']
 const evenNumbers = [2, 4, 6, 8, 10]
 
+/* display1 function is being used to only display the elements position in an array */
+
 display1 = function(arr){
   for(let i = 0; i < arr.length; i++){
     console.log(`This is position ${i} of this array`)
@@ -10,6 +12,8 @@ display1 = function(arr){
 
 display1(trees)
 display1(evenNumbers)
+
+/* display2 function is being used to display the elements and the position of those elements in an array */
 
 display2 = function(arr){
   for(let i = 0; i < arr.length; i++){
