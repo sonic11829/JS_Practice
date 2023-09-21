b2f0e793f0b1b136ba0c6e07549c5c56dc8527a5
sonic11829 <sethneal15@ymail.com>
2023-09-20 21:27:02 -0400

    made first display function and tested, works

---
+++ loops/loopsDay03.js
@@ -0,0 +1,12 @@
+
+const trees = ['oak', 'birch', 'maple', 'dog', 'pine']
+const evenNumbers = [2, 4, 6, 8, 10]
+
+display1 = function(arr){
+  for(let i = 0; i < arr.length; i++){
+    console.log(`This is position ${i} of this array`)
+  }
+}
+
+display1(trees)
+display1(evenNumbers)
