c19e9d76c3e0e19d0b8b0defbbf0a03594d8f236
sonic11829 <sethneal15@ymail.com>
2023-09-25 21:08:10 -0400

    created day_04 and made display1 function

---
+++ loops/loopsDay04.js
@@ -0,0 +1,15 @@
+
+const firstNameBoy = ['Seth', 'Cody', 'Josey', 'Gabe', 'Eli']
+const firstNameGirl = ['Amber', 'Hannah', 'Mae', 'Grace', 'Micah']
+const lastName = ['Neal', 'Piper', 'Carlson']
+
+display1 = function(arr1){
+  for(let i = 0; i < arr1.length; i++){
+    console.log(arr1[i])
+  }
+}
+
+display1(firstNameBoy)
+display1(firstNameGirl)
+display1(lastName)
+
