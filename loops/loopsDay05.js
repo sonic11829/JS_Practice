847cf6f792b0cdfb76eb444fe3623daa29c5fd30
sonic11829 <sethneal15@ymail.com>
2023-09-26 21:23:09 -0400

    added comments

--- loops/loopsDay05.js
+++ loops/loopsDay05.js
@@ -1,5 +1,7 @@
+
 const integer = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+/* this add function takes the number of the index and adds it to the next indexed position */
 add = function(arr){
   for(let i = 0; i < arr.length; i++){
     total = arr[i] + i+1
@@ -9,6 +11,7 @@ add = function(arr){
 
 add(integer)
 
+/* this subract function takes the next indexed position and subtracts it from the current number of that index */
 subtract = function(arr){
   for(let i = arr.length; i >= 0; i--){
     total = i+1 - arr[i]
