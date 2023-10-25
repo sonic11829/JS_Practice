43506e3bc05dad8cc89bccfc16a6012f73ba59ec
sonic11829 <sethneal15@ymail.com>
2023-10-25 19:46:54 -0400

    coded first part of loopsDay07.js

--- loops/loopsDay07.js
+++ loops/loopsDay07.js
@@ -1,5 +1,9 @@
 
-const rifle = ["M16", "AK-47", "Galil", "Aug", "G3A3"]
+const rifle = ["M4A1", "AK-47", "STG-44", "M14", "M16a4", "G3", "G36C"]
+const sniper = ["Dragunov", "M21", "R700", "M40A3", "Barret .50cal"]
+const shotgun = ["M1014", "W1200"]
+const lmg = ["M249", "RPD", "M60E4"]
+const smg = ["MP5", "AK-47u", "P90", "Skorpion", "Mini-Uzi"]
 
 primaryWeapon = function(array){
   for(let i = 0; i < array.length; i++){
@@ -8,3 +12,7 @@ primaryWeapon = function(array){
 }
 
 primaryWeapon(rifle)
+primaryWeapon(sniper)
+primaryWeapon(shotgun)
+primaryWeapon(lmg)
+primaryWeapon(smg)
