bda5506b2396115d820750f274a65e2ac86e0ae7
sonic11829 <sethneal15@ymail.com>
2023-10-26 20:40:44 -0400

    created day08 file and started code

---
+++ loops/loopsDay08.js
@@ -0,0 +1,7 @@
+
+const hat = ["ball cap", "snow hat", "beenie", "none"]
+const shirt = ["t-shirt", "sweater", "\'wife beater\'", "none"]
+const pant = ["jeens", "snow pants", "shorts", "swim suit"]
+const shoes = ["tennis shoes", "boots", "velcro shoes", "swim shoes"]
+const coat = ["jacket", "parka", "vest", "life jacket"]
+
