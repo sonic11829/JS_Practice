af6867d5345158d31074a017dd718a4e86e54dae
sonic11829 <sethneal15@ymail.com>
2023-10-26 20:49:38 -0400

    created functions to only allow a specific array

--- loops/loopsDay08.js
+++ loops/loopsDay08.js
@@ -5,3 +5,42 @@ const pant = ["jeens", "snow pants", "shorts", "swim suit"]
 const shoes = ["tennis shoes", "boots", "velcro shoes", "swim shoes"]
 const coat = ["jacket", "parka", "vest", "life jacket"]
 
+outfitHat = function(arr){
+  if(arr !== hat){
+    return 0
+  } else {
+    console.log(arr)
+  }
+}
+
+outfitShirt = function(arr){
+  if(arr !== shirt){
+    return 0
+  } else {
+    console.log(arr)
+  }
+}
+
+outfitPant = function(arr){
+  if(arr !== pant){
+    return 0
+  } else {
+    console.log(arr)
+  }
+}
+
+outfitShoes = function(arr){
+  if(arr !== shoes){
+    return 0
+  } else {
+    console.log(arr)
+  }
+}
+
+outfitCoat = function(arr){
+  if(arr !== coat){
+    return 0
+  } else {
+    console.log(arr)
+  }
+}
